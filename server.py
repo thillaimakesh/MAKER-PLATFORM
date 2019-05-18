f12d835250f1833f8be7fa123f87afa52da23837
thillaimakesh <34833235+thillaimakesh@users.noreply.github.com>
2019-05-18 17:43:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,22 +5,19 @@ from flask import Response
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-@app.route('/workmate',methods = ['POST','GET'])
-def tok():
-    value = request.args.get("value")
-    key = request.args.get("key")
-    comp = request.args.get("company")
-    print(comp)
-    method = request.args.get("method")
-    get= {}
-    get[method]= {
-    key: value
-    }
-    s= json.dumps(get)
-     f = open(comp + '.json',"a")
-    f.write(str(s))
-    f.close()
-    return "done"
+def process_key_val(key,val):
+    count = 0
+    ans = ""
+    k = key.split(",")
+    v = val.split(",")
+    for c1 in key.split(","):
+        count += 1
+    for i in range(0,count-1):
+        if i + 1 != count-1:
+           ans += "'" + k[i] + "'" + ":" + "'" + v[i] + "'" + ","
+        if i + 1 == count-1:
+           ans += "'" + k[i] + "'" + ":" + "'" + v[i] + "'"
+        return '{' + ans + '}'
 @app.route('/platform',methods = ['POST','GET'])
 def tik():
     value = request.args.get("value")
@@ -28,23 +25,25 @@ def tik():
     content = f.read()
     return content
 @app.route('/json',methods = ['POST','GET'])
-str_key = request.args.get("key")
-str_val = request.args.get("value")
-def process_key_val(key,val):
-    count = 0
-    ans = ""
-    k = str_key.split(",")
-    v = str_val.split(",")
-    for c1 in key.split(","):
-        count += 1
-         for i in range(0,count):
-        if i + 1 != count:
-            ans += "'" + k[i] + "'" + ":" + "'" + v[i] + "'" + ","
-        if i + 1 == count:
-            ans += "'" + k[i] + "'" + ":" + "'" + v[i] + "'"
-    return '{' + ans + '}'
-print process_key_val(str_key,str_val)
+def done():
+    str_key = request.args.get("key")
+@app.route('/platform',methods = ['POST','GET'])
+def tik():
+    value = request.args.get("value")
+    f = open(value+".json","r")
+    content = f.read()
+    return content
+@app.route('/json',methods = ['POST','GET'])
+def done():
+    str_key = request.args.get("key")
+    str_val = request.args.get("value")
+    val = process_key_val(str_key,str_val)
+    comp = request.args.get("company")
+    print(comp)
+    f = open(comp + '.json',"a")
+    f.write(str(val))
+    f.close()
+    return 'done'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
 
